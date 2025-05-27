4adf1a31fb83d30ce54a6099a9f57ebe6724d1a2
rtalwar-silk <rtalwar@silk-shopping.com>
2025-05-26 20:39:29 -0700

    update officer info

--- acm_website/src/pages/AboutPage.tsx
+++ acm_website/src/pages/AboutPage.tsx
@@ -4,7 +4,6 @@ import miseokImage from '../assets/alumni/miseok kim.jpg';
 import juliaImage from '../assets/alumni/julia bian.jpeg';
 import nishImage from '../assets/alumni/nish.jpeg';
 import chaseImage from '../assets/alumni/chase feng.jpeg';
-import backgroundImage from '../assets/aboutus_background.webp';
 import '../styles/FlipCard.css';
 
 interface AboutPageProps {
@@ -46,32 +45,32 @@ const AboutPage: React.FC<AboutPageProps> = ({ navigateTo, error }) => {
   // Leadership data
   const leadershipData: PersonData[] = [
     { 
-      name: "Alan Li", 
-      role: "President", 
-      bio: "2026",
-      linkedin: "https://www.linkedin.com/in/alanli6/",
-      imagePath: "/images/team/alan.jpg"
-    },
-    { 
-      name: "Elizabeth Fu", 
-      role: "Vice President", 
-      bio: "2027",
-      linkedin: "https://www.linkedin.com/in/elizabeth-fu",
-      imagePath: "/images/team/elizabeth.jpg"
-    },
-    { 
       name: "Spencer Ye", 
-      role: "Treasurer", 
+      role: "President", 
       bio: "2027",
       linkedin: "https://www.linkedin.com/in/spencerye/",
       imagePath: "/images/team/spencer.jpg"
     },
     { 
-      name: "Christian Byun", 
+      name: "Elizabeth Fu", 
+      role: "Vice President", 
+      bio: "2027",
+      linkedin: "https://www.linkedin.com/in/elizabeth-f-773b78244/",
+      imagePath: "/images/team/elizabeth.jpg"
+    },
+    { 
+      name: "Ria Talwar", 
+      role: "Treasurer", 
+      bio: "2027",
+      linkedin: "https://www.linkedin.com/in/ria-talwar-cs2027/",
+      imagePath: "/images/team/ria.jpg"
+    },
+    { 
+      name: "Mischa Kumar", 
       role: "Secretary", 
       bio: "2028",
-      linkedin: "https://www.linkedin.com/in/christian-byun",
-      imagePath: "/images/team/christian.jpg"
+      linkedin: "https://www.linkedin.com/in/mischa-kumar-274631298/",
+      imagePath: "/images/team/mischa.jpg"
     },
     { 
       name: "Timothy Lin", 
@@ -93,6 +92,27 @@ const AboutPage: React.FC<AboutPageProps> = ({ navigateTo, error }) => {
       bio: "2026",
       linkedin: "https://www.linkedin.com/in/tianai-yue-70981523b/",
       imagePath: "/images/team/tianai.jpg"
+    },
+    { 
+      name: "Alan Li", 
+      role: "General Officer", 
+      bio: "2026",
+      linkedin: "https://www.linkedin.com/in/alanli6/",
+      imagePath: "/images/team/alan.jpg"
+    },
+    { 
+      name: "Vrinda Sehgal", 
+      role: "General Officer", 
+      bio: "2028",
+      linkedin: "https://www.linkedin.com/in/vrinda-sehgal-0000000000/",
+      imagePath: "/images/team/vrinda.jpg"
+    },
+    { 
+      name: "Vicki Chen", 
+      role: "General Officer", 
+      bio: "2028",
+      linkedin: "https://www.linkedin.com/in/vickichenn/",
+      imagePath: "/images/team/vicki.jpg"
     }
   ];
 

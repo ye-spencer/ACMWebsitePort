828e6c237f4d112ea6f8dd5863ff836c306a73b3
riatalwar <ria.talwar2006@gmail.com>
2025-07-08 15:51:14 -0700

    add logo to navbar

--- acm_website/src/components/Navbar.tsx
+++ acm_website/src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import '../styles/NavBar.css';
 import { auth } from '../firebase/config';
 import { onAuthStateChanged } from "firebase/auth";
+import logo from '../assets/logo.png';
 
 const Navbar: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -17,13 +18,18 @@ const Navbar: React.FC = () => {
 
   return (
       <div className="navbar">
-        <Link to="/" className="nav-links">Home</Link>
-        <Link to="/about" className="nav-links">About Us</Link>
-        <Link to="/events" className="nav-links">Events</Link>
-        <Link to="/booking" className="nav-links">Book Lounge</Link>
-        <Link to={isAdmin ? '/admin' : isLoggedIn ? '/profile' : '/login'} className="nav-links">
-          {isAdmin ? 'Admin' : isLoggedIn ? 'Profile' : 'Login'}
+        <Link to="/" className="navbar-logo">
+          <img src={logo} alt="ACM Logo" className="logo" />
         </Link>
+        <div className="nav-links-container">
+          <Link to="/" className="nav-links">Home</Link>
+          <Link to="/about" className="nav-links">About Us</Link>
+          <Link to="/events" className="nav-links">Events</Link>
+          <Link to="/booking" className="nav-links">Book Lounge</Link>
+          <Link to={isAdmin ? '/admin' : isLoggedIn ? '/profile' : '/login'} className="nav-links">
+            {isAdmin ? 'Admin' : isLoggedIn ? 'Profile' : 'Login'}
+          </Link>
+        </div>
       </div>
   );
 };
